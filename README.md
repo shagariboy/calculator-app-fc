1edc4037b7384f28d20ae29c619c9bd14731f45c
Nnamdi Ezeh <nnamdiezeh0@gmail.com>
2022-12-26 23:43:19 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,56 @@
+# Tip calculator app Project
+
+## A Frontend Mentor Challenge
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+- [Author](#author)
+
+## Overview
+
+Build the app according to the given design. Choose specific tools that is handy and customized the design freely.
+
+### The challenge
+
+User's flow:
+
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Calculate the correct tip and total cost of the bill per person
+
+### Links
+
+- Solution URL: [Github](https://github.com/shagariboy/interactive-rating-component-main)
+- Live Site URL: [Live Site](https://delightful-sunburst-264113.netlify.app/)
+
+## My Process
+
+- Utilized ClickUp to break down the workflow
+
+### Built with
+
+- HTML
+- CSS
+- SaSS
+- JavasScript
+- Mobile-first workflow
+
+### What I learned
+
+I becoming more confident in my JavaScript.
+
+
+## Screenshot
+
+![TipCalculator](desktop-preview.png)
+![TipCalculator](mobile-design.jpg)
+
+## Author
+
+- Frontend Mentor - [@shagariboy](https://www.frontendmentor.io/profile/shagariboy)
---
+++ js/app.js
@@ -0,0 +1,118 @@
+const bill = document.getElementById('bill');
+const tipBtn = document.querySelectorAll('.tip');
+const tipCustom = document.getElementById('tip');
+const people = document.getElementById('people');
+const errorMsg = document.querySelector('.error-note');
+const results = document.querySelectorAll('.value');
+const resetBtn = document.querySelector('.reset');
+
+bill.addEventListener('input', setBillValue);
+tipBtn.forEach(btn => {
+    btn.addEventListener('click', handleClick);
+});
+tipCustom.addEventListener('input', setTipCustomValue);
+people.addEventListener('input', setPeopleValue);
+resetBtn.addEventListener('click', reset);
+
+let billValue = 0.0;
+let tipValue = 0.15;
+let peopleValue = 1;
+
+function validateFloat(s) {
+    var rgx = /^[0-9]*\.?[0-9]*$/;
+    return s.match(rgx);
+}
+
+function validateInt(s) {
+    var rgx = /^[0-9]*$/;
+    return s.match(rgx);
+}
+
+function setBillValue() {
+    if (bill.value.includes(',')) {
+        bill.value = bill.value.replace(',', '.');
+    }
+
+    if (!validateFloat(bill.value)) {
+        bill.value = bill.value.substring(0, bill.value.length - 1);
+    }
+
+    billValue = parseFloat(bill.value);
+
+    calculateTip();
+}
+
+function handleClick(event) {
+    tipBtn.forEach(btn => {
+        btn.classList.remove('btn-active');
+
+        if (event.target.innerHTML == btn.innerHTML) {
+            btn.classList.add('btn-active');
+            tipValue = parseFloat(btn.innerHTML) / 100;
+        }
+    });
+
+    tipCustom.value = '';
+
+    calculateTip();
+}
+
+function setTipCustomValue() {
+    if (!validateInt(tipCustom.value)) {
+        tipCustom.value = tipCustom.value.substring(0, tipCustom.value.length - 1);
+    }
+
+    tipValue = parseFloat(tipCustom.value / 100);
+
+    tipBtn.forEach(btn => {
+        btn.classList.remove('btn-active');
+    });
+
+    if (tipCustom.value !== '') {
+        calculateTip();
+    }
+}
+
+function setPeopleValue() {
+    if (!validateInt(people.value)) {
+      people.value = people.value.substring(0, people.value.length - 1);
+    }
+
+    peopleValue = parseFloat(people.value);
+
+    if (peopleValue <= 0){
+        errorMsg.classList.add('show-error-note');
+        setTimeout(function () {
+            errorMsg.classList.remove('show-error-note');
+        }, 3000);
+        people.style.border = "2px solid #e17456";
+    }
+    else if (peopleValue >= 0){
+        people.style.border = "2px solid #26c0ab";
+    }
+
+    console.log(peopleValue);
+
+    calculateTip();
+}
+
+function calculateTip() {
+    if (peopleValue >= 1) {
+        let tipAmount = billValue * tipValue / peopleValue;
+        let total = billValue * (tipValue + 1) / peopleValue;
+        results[0].innerHTML = '$' + tipAmount.toFixed(2);
+        results[1].innerHTML = '$' + total.toFixed(2);
+
+    }
+}
+
+function reset() {
+    bill.value = '0';
+    setBillValue();
+
+    tipBtn[2].click();
+
+    people.value = '0';
+    setPeopleValue();
+}
+
